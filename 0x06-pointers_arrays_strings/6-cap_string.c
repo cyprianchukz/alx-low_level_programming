102739c33999b36a003a0f22e81c00b4db0956f3
cyprianchukz <cyprianchukz@gmail.com>
2023-07-27 13:15:00 -0700

    more pointers

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
 * cap_string - Capitalizes all words of a string.
+* @str: The string to be capitalized.
 *
 * Return: A pointer to the changed string.
 */
@@ -27,4 +28,6 @@ str[index - 1] == '}' ||
 index == 0)
 str[index] -= 32;
 index++;
+}
+return (str);
 }
