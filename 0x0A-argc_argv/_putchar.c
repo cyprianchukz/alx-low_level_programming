362ce863e64f12a7465ee15ab47aa833babf3f67
cyprianchukz <cyprianchukz@gmail.com>
2023-08-04 14:10:39 -0700

    argc argv

--- 0x0A-argc_argv/_putchar.c
+++ 0x0A-argc_argv/_putchar.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 /**
-* main - prints the name of the program
-* @argc: number of arguments
-* @argv: array of arguments
+* _putchar - writes the character c to stdout
+* @c:Ther character to print
 *
-* Return: Always 0 (Success)
+* Return: On success 1.
+* On error, -1 is returned and errno is set appropriately.
 */
-int main(int argc __attribute__((unused)), char *argv[])
+int _putchar(char c)
 {
-printf("%s\n", *argv);
-return (0);
+return (write(1, &c, 1));
 }
